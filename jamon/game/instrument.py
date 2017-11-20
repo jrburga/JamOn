163f150045753686c263ffa41d7a4603089b8e7b
piotti <piotti@mit.edu>
2017-11-20 00:00:02 -0500

    fixed instrument class

--- jamon/game/instrument.py
+++ jamon/game/instrument.py
@@ -1,24 +1,25 @@
 from game import GameObject
-from common.fluidsynth import Synth
+from common.synth import Synth
 
 class InstrumentManager(GameObject):
 
 	def __init__(self):
-		super(Instrument, self).__init__()
-		self.synth = Synth("../data/FluidR3_GM.sf2")
+		super(InstrumentManager, self).__init__()
+		self.synth = Synth("data/FluidR3_GM.sf2")
 		self.add_generator(self.synth)
 		self.inst_count = 0
+
+		self.instruments = []
 
 
 	# Adds instrument to manager. Each instrument is added to 
 	# a new channel.
-	# Inputs:
-	#		patch -- tuple of (bank, program)
-	# Returns the instrument's channel number
-	def add_instrument(self, patch):
+	def add(self, ins):
 		self.inst_count += 1
-		self.synth.program(self.inst_count, *patch)
-		return self.inst_count
+		self.instruments.append(ins)
+		ins.manager = self
+		ins.channel = self.inst_count
+		self.synth.program(self.inst_count, ins.patch, ins.notes)
 
 	def noteon(self, *args):
 		self.synth.noteon(*args)
@@ -26,8 +27,6 @@ class InstrumentManager(GameObject):
 	def noteoff(self, *args):
 		self.synth.noteoff(*args)
 
-
-IM = InstrumentManager()
 
 
 INSTRUMENTS = {
@@ -39,14 +38,22 @@ class Instrument(object):
 	def __init__(self, inst):
 		super(Instrument, self).__init__()
 		(self.patch, self.notes) = INSTRUMENTS[inst]
-		self.channel = IM.add_instrument(self.patch)
 		self.vel = 75
+		self.manager = None
 
 	def note_on(self, lane):
+		assert 0 <= lane < len(self.notes)
+		if self.manager is None:
+			print "instrument not added to manager"
+			raise Exception
 		pitch = self.notes[lane]
-		IM.noteon(self.channel, pitch, self.vel)
+		self.manager.noteon(self.channel, pitch, self.vel)
 
 
 	def note_off(self, note):
+		assert 0 <= lane < len(self.notes)
+		if self.manager is None:
+			print "instrument not added to manager"
+			raise Exception
 		pitch = self.notes[lane]
-		IM.noteoff(self.channel, pitch)
+		self.manager.noteoff(self.channel, pitch)
