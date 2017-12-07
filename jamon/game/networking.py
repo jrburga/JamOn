0343539ac46a666b72b09a3bd2c9c32bc4d2471f
jrburga <jrburga@mit.edu>
2017-12-07 14:35:46 -0500

    added networking capabilities

---
+++ jamon/game/networking.py
@@ -0,0 +1,66 @@
+from game import GameObject
+from random import choice
+
+member_names = ['Rudolf', 'Dasher', 'Prancer', 'Eran', 'Berry', 'Dancer', 'Vixen', 'Donner', 'Cupid']
+
+def _default_callback(message):
+	if message.data['success']:
+		return message.data['result']
+
+class ClientObject(GameObject):
+	'''
+	Light weight wrapper to interface with the Client
+	'''
+	def __init__(self, client, username=None):
+		super(ClientObject, self).__init__()
+		self.client = client
+		self.username = username if username else choice(member_names)
+		self.band_members = []
+		self.client.register_tether_callback(self.on_action)
+
+	@property 
+	def id(self):
+		return self.client.id
+
+	@property
+	def is_host(self):
+		return self.client.is_host
+
+	@property
+	def addr_str(self):
+		return self.client.ip
+
+	def info(self):
+		return {'username': self.username, 
+				'id': self.id, 
+				'is_host': self.is_host, 
+				'addr_str': self.addr_str}
+
+	def post_info(self, info_name, info, callback=_default_callback):
+		'''
+		Blocking. Use callback to get responses
+		'''
+		return self.client.post(info_name, info, callback)
+
+	def get_info(self, info_name, identifier, callback=_default_callback):
+		return self.client.get(info_name, identifier, callback)
+
+	def connect(self, ip):
+		self.client.connect(ip)
+
+	def disconnect(self):
+		self.client.disconnect()
+
+	def on_action(self, action):
+		# print 'on action called', action
+		self.trigger_event(action['event_type'], **action['action'])
+
+	def send_action(self, event_type, **args):
+		'''
+		Non blocking. Actions sent to the server
+		will be sent back to the other clients
+		who will then execute those actions respectively 
+		via their event handlers
+		'''
+		action = {'event_type': event_type, 'action': args}
+		self.client.send('action', action)
---
+++ server/store.py
@@ -0,0 +1,28 @@
+class Store(dict):
+	def __init__(self, **kwargs):
+		super(Store, self).__init__(kwargs)
+
+	def __getattr__(self, name):
+		return self[name]
+
+	def __setattr__(self, name, value):
+		assert False, 'cannot change store values'
+
+	def __setitem__(self, key, value):
+		assert False, 'cannot change store values'
+
+class MemberStore(Store):
+	def __init__(self, username, id, ip, is_host):
+		store_dict = {'username': username, 
+					 'id': id, 'ip': ip, 
+					 'is_host': is_host}
+		super(MemberStore, self).__init__(**store_dict)
+
+class PatternStore(Store):
+	def __init__(self, member_id, pattern):
+		store_dict = {'member_id': member_id, 'pattern': pattern}
+		super(PatternStore, self).__init__(**store_dict)
+
+if __name__ == '__main__':
+	bm = MemberStore('jake', 1234, '0.0.0.0', True)
+	print bm.is_host
