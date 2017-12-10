6dc0f217e17ed2cee0760aff7527e09090e1bb30
Tim Plump <tiplump@gmail.com>
2017-12-10 18:22:53 -0500

    fixed full screen for mac

--- jamon/game/window.py
+++ jamon/game/window.py
@@ -9,18 +9,27 @@ if plat == 'Windows':
 	h = GetSystemMetrics(1)
 elif plat == 'Darwin':
 	# MAC OSX
-	import AppKit
-	screen = AppKit.NSScreen.screens()[0]
-	w = int(screen.frame().size.width)
-	h = int(screen.frame().size.height)
+
+	# import AppKit
+	# screen = AppKit.NSScreen.screens()[0]
+	# w = int(screen.frame().size.width)
+	# h = int(screen.frame().size.height)
+	# print w,h
+
+	import subprocess
+	import re
+	results = str(subprocess.Popen(['system_profiler SPDisplaysDataType'],stdout=subprocess.PIPE, shell=True).communicate()[0])
+	res = re.search('Resolution: \d* x \d*', results).group(0).split(' ')
+	width, height = int(res[1]), int(res[3])
+	
 else:
 	raise UnsupportedSystemException
 
 
 class Window:
 
-	width = w
-	height = h
+	width = width
+	height = height
 
 
 class UnsupportedSystemException(Exception):
